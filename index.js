2ebff1c07a189b17124147c3795a093181325c33
razimbrao <rafaelozimbrao@gmail.com>
2022-07-05 17:36:45 -0300

    desafio completado

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // Variaveis:
 let allTasks = [];
 const tasksFromLocalStorage = JSON.parse(localStorage.getItem("taskList"));
@@ -7,24 +8,33 @@ const input = document.querySelector("#inputText");
 const btnInsert = document.querySelector("#btnInsert");
 const btnClear = document.querySelector("#btnRemoveAll");
 const taskList = document.querySelector("#listItems");
+const temp = document.querySelector("#temp");
+const maxTemp = document.querySelector("#max-temp");
+const minTemp = document.querySelector("#min-temp");
 
 //Eventos:
 btnInsert.addEventListener("click", addTask);
 btnClear.addEventListener("click", cleanAll);
 
-input.addEventListener("keypress", (e) =>{
-    if(e.key === "Enter") addTask();    
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") addTask();
 });
 
-taskList.addEventListener(
-  "dblclick", (e) =>{ e.target.className += " completo";}
+taskList.addEventListener( "dblclick", (e) => { 
+  e.target.className += " completo";
+  let index = allTasks.findIndex(
+    (task) => task.taskName === e.target.textContent
+  );
+  allTasks[index].isComplete = true;
+  saveToLocalStorage(allTasks);
+}
 );
 
 if (tasksFromLocalStorage) {
   allTasks = [...tasksFromLocalStorage];
 }
 renderTasks(allTasks);
-
+getWeatherData();
 // Funções:
 function addTask() {
   if (input.value === "") return;
@@ -36,7 +46,7 @@ function addTask() {
   li.appendChild(document.createTextNode(task));
 
   taskList.appendChild(li);
-  allTasks.push(task);
+  allTasks.push({taskName: task, isComplete: false});
   saveToLocalStorage(allTasks);
   input.value = "";
 }
@@ -56,11 +66,37 @@ function saveToLocalStorage(tasks) {
 
 function renderTasks(tasks) {
   list = "";
-
   for (let i = 0; i < tasks.length; i++) {
     list += `
-        <li class="item-lista">${tasks[i]}</li>
+        <li class="item-lista ${tasks[i].isComplete ? "completo" : ""}">
+        ${tasks[i].taskName}
+        </li>
         `;
   }
   taskList.innerHTML = list;
+}
+
+async function getWeatherData() {
+  // função assíncrona porque se deve esperar uma resposta, que não necessariamente vai ser instantânea
+  const apiKey = "47e811d46816fb69a6cd719759c80f18"; // código da API retirado da dashboard do site
+  const units = "metric"; // opção da API, para usar a unidade padrão (ºC)
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=-21.7642&lon=-43.3496&appid=${apiKey}&units=${units}&lang=pt_br`; // URL, para ficar fácil de alterar
+  
+  await fetch(url, {method: "GET"})
+    .then((response) => {
+      if (!response.ok){
+        return new Error("falha a requisição");
+      }
+      if(response.status === 404){
+        return new Error("não encontrou qualquer resultado");
+      }
+      return response.json().then((text) => {
+        temp.textContent = "Temperatura atual: " +text.main.temp;
+        maxTemp.textContent = "Temperatura máxima: " +text.main.temp_max;
+        minTemp.textContent = "Temperatura mínima: " +text.main.temp_min;
+      });
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 }
